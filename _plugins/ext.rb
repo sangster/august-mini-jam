bbda4ab261319a7e02165193378c3b6f5d2fd596
Jon Sangster <jon@ertt.ca>
2016-08-12 18:16:38 -0300

    initial commit

---
+++ _plugins/ext.rb
@@ -0,0 +1,7 @@
+################################################################################
+# This file configures the extra plugins that Jekyll uses to build this site
+#
+# See
+#   - https://github.com/jekyll/jekyll-assets
+################################################################################
+require 'jekyll-assets'
